ef28b8311fcc20882e03044fee75434de6747749
jvolcy <jerry.volcy@gmail.com>
2023-01-14 21:18:49 -0500

    Added quad as a canvas

--- Assets/Prefabs/Skeleton/Scripts/ModelController.cs
+++ Assets/Prefabs/Skeleton/Scripts/ModelController.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 public class ModelController : MonoBehaviour
 {
     public Material NormalBoneMaterial;
     public Material HighlightedBoneMaterial;
+    public TMP_Text PartName;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +26,13 @@ public class ModelController : MonoBehaviour
     {
         MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
         meshRenderer.material = HighlightedBoneMaterial;
+        PartName.text = gameObject.name;
     }
     void UnHighlightObject(GameObject gameObject)
     {
         MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
         meshRenderer.material = NormalBoneMaterial;
+        PartName.text = "---";
     }
     public void OnHoverEnter(HoverEnterEventArgs args)
     {
